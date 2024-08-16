028ca954ded51a78893e3513a49ab75d5dda40ce
stiltondotcom <mcukan@gmail.com>
2024-08-16 13:35:53 -0400

    used ChatGPT to switch API to RapidAPI SeekingAlpha

--- backtester.py
+++ backtester.py
@@ -4,14 +4,22 @@ author - Kaneel Senevirathne
 date - 1/13/2022
 """
 
+import requests, time, re, os
+import matplotlib.pyplot as plt
+
 import numpy as np
 from stock_utils.simulator import simulator
 from stock_utils.stock_utils import get_stock_price
 from models import lr_inference
 from datetime import datetime
 from datetime import timedelta
-from td.client import TDClient
+
 import pandas as pd
+
+from scipy.signal import argrelextrema
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import MinMaxScaler
+
 from models.lr_inference import LR_v1_predict, LR_v1_sell
 import warnings
 from collections import OrderedDict
@@ -19,6 +27,172 @@ warnings.filterwarnings("ignore")
 import os
 import pickle
 from tqdm import tqdm
+
+# Seeking Alpha API - Replace with your actual API key
+SA_API_KEY = 'XXXXX' ### your Seeking Alpha API key
+
+def timestamp(dt):
+    epoch = datetime.utcfromtimestamp(0)
+    return int((dt - epoch).total_seconds() * 1000)
+
+def linear_regression(x, y):
+    """
+    performs linear regression given x and y. outputs regression coefficient
+    """
+    lr = LinearRegression()
+    lr.fit(x, y)
+    
+    return lr.coef_[0][0]
+
+def n_day_regression(n, df, idxs):
+    """
+    n day regression.
+    """
+    _varname_ = f'{n}_reg'
+    df[_varname_] = np.nan
+
+    for idx in idxs:
+        if idx > n:
+            y = df['close'][idx - n: idx].to_numpy()
+            x = np.arange(0, n)
+            y = y.reshape(y.shape[0], 1)
+            x = x.reshape(x.shape[0], 1)
+            coef = linear_regression(x, y)
+            df.loc[idx, _varname_] = coef
+            
+    return df
+
+def normalized_values(high, low, close):
+    """
+    normalize the price between 0 and 1.
+    """
+    epsilon = 10e-10
+    high = high - low
+    close = close - low
+    return close/(high + epsilon)
+
+def get_stock_price(stock, date):
+    """
+    returns the stock price given a date using Seeking Alpha API
+    """
+    start_date = (date - timedelta(days=10)).strftime('%Y-%m-%d')
+    end_date = date.strftime('%Y-%m-%d')
+    
+    url = f'https://seeking-alpha.p.rapidapi.com/market/get-chart'
+    headers = {
+        'X-RapidAPI-Key': SA_API_KEY,
+        'X-RapidAPI-Host': 'seeking-alpha.p.rapidapi.com'
+    }
+    query = {
+        'symbol': stock,
+        'period': '10d',
+        'interval': '1d'
+    }
+
+    results = requests.get(url, headers=headers, params=query)
+    data = results.json()
+    
+    try:
+        # Assuming the Seeking Alpha API returns similar structured data
+        prices = [day['close'] for day in data['data']]
+        return prices[-1]
+    except:
+        pass
+    
+def get_data(sym, start_date=None, end_date=None, n=10):
+    """
+    Fetch historical stock data using Seeking Alpha API
+    """
+    if not start_date:
+        start_date = datetime(2007, 1, 1)
+    if not end_date:
+        end_date = datetime(2020, 12, 31)
+
+    url = f'https://seeking-alpha.p.rapidapi.com/market/get-chart'
+    headers = {
+        'X-RapidAPI-Key': SA_API_KEY,
+        'X-RapidAPI-Host': 'seeking-alpha.p.rapidapi.com'
+    }
+    query = {
+        'symbol': sym,
+        'period': '10y',  # Adjust as needed
+        'interval': '1d',
+        'from': start_date.strftime('%Y-%m-%d'),
+        'to': end_date.strftime('%Y-%m-%d')
+    }
+
+    results = requests.get(url, headers=headers, params=query)
+    data = results.json()
+    
+    # Assuming the Seeking Alpha API returns similar structured data
+    df = pd.DataFrame(data['data'])
+    df['date'] = pd.to_datetime(df['date'])
+
+    df['normalized_value'] = df.apply(lambda x: normalized_values(x.high, x.low, x.close), axis=1)
+    df['loc_min'] = df.iloc[argrelextrema(df.close.values, np.less_equal, order=n)[0]]['close']
+    df['loc_max'] = df.iloc[argrelextrema(df.close.values, np.greater_equal, order=n)[0]]['close']
+
+    idx_with_mins = np.where(df['loc_min'] > 0)[0]
+    idx_with_maxs = np.where(df['loc_max'] > 0)[0]
+    
+    return df, idx_with_mins, idx_with_maxs
+
+def create_train_data(stock, start_date=None, end_date=None, n=10):
+    data, idxs_with_mins, idxs_with_maxs = get_data(stock, start_date, end_date, n)
+    
+    data = n_day_regression(3, data, list(idxs_with_mins) + list(idxs_with_maxs))
+    data = n_day_regression(5, data, list(idxs_with_mins) + list(idxs_with_maxs))
+    data = n_day_regression(10, data, list(idxs_with_mins) + list(idxs_with_maxs))
+    data = n_day_regression(20, data, list(idxs_with_mins) + list(idxs_with_maxs))
+  
+    _data_ = data[(data['loc_min'] > 0) | (data['loc_max'] > 0)].reset_index(drop=True)
+    _data_['target'] = [1 if x > 0 else 0 for x in _data_.loc_max]
+    
+    cols_of_interest = ['volume', 'normalized_value', '3_reg', '5_reg', '10_reg', '20_reg', 'target']
+    _data_ = _data_[cols_of_interest]
+    
+    return _data_.dropna(axis=0)
+
+def create_test_data_lr(stock, start_date=None, end_date=None, n=10):
+    data, _, _ = get_data(stock, start_date, end_date, n)
+    idxs = np.arange(0, len(data))
+    
+    data = n_day_regression(3, data, idxs)
+    data = n_day_regression(5, data, idxs)
+    data = n_day_regression(10, data, idxs)
+    data = n_day_regression(20, data, idxs)
+    
+    cols = ['close', 'volume', 'normalized_value', '3_reg', '5_reg', '10_reg', '20_reg']
+    data = data[cols]
+
+    return data.dropna(axis=0)
+
+def predict_trend(stock, _model_, start_date=None, end_date=None, n=10):
+    data, _, _ = get_data(stock, start_date, end_date, n)
+    
+    idxs = np.arange(0, len(data))
+    data = n_day_regression(3, data, idxs)
+    data = n_day_regression(5, data, idxs)
+    data = n_day_regression(10, data, idxs)
+    data = n_day_regression(20, data, idxs)
+        
+    data['pred'] = np.nan
+
+    cols = ['volume', 'normalized_value', '3_reg', '5_reg', '10_reg', '20_reg']
+    x = data[cols]
+
+    scaler = MinMaxScaler()
+    x = scaler.fit_transform(x)
+
+    for i in range(x.shape[0]):
+        try:
+            data['pred'][i] = _model_.predict(x[i, :])
+        except:
+            data['pred'][i] = np.nan
+
+    return data
+
+
 
 class backtester(simulator):
 
